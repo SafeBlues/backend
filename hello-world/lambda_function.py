a43119448315664b9750d47c4d74f200f9b06934
Aapeli <git@aapelivuorinen.com>
2020-06-17 17:57:23 +1000

    Initial commit

---
+++ hello-world/lambda_function.py
@@ -0,0 +1,10 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            "success": True
+        })
+    }
