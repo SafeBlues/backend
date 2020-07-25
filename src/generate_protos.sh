c2611b55a5de66db909f04defca3fc9784bc6a75
Aapeli <git@aapelivuorinen.com>
2020-07-26 09:21:44 +1000

    Add descriptor set

--- src/generate_protos.sh
+++ src/generate_protos.sh
@@ -1,1 +1,1 @@
-find . -name '*.proto' | protoc -I. --python_out=. --grpc_python_out=. --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) $(xargs)
+find . -name '*.proto' | protoc -I. --python_out=. -o sb.pb --grpc_python_out=. --plugin=protoc-gen-grpc_python=$(which grpc_python_plugin) $(xargs)
